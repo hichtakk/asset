59f8bc002c228ec54fcdbc3935ce43fa1de06121
Hirotaka Ichikawa <hichtakk@gmail.com>
2016-01-16 19:24:23 -0800

    create asset view

--- assetdb/views.py
+++ assetdb/views.py
@@ -68,6 +68,15 @@ def show_model_detail(request):
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
 
 
+@view_config(route_name='show_asset_detail', renderer='templates/asset_detail.html')
+def show_asset_detail(request):
+    try:
+        asset_id = request.matchdict['asset_id']
+        return {'asset_id': asset_id}
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+
+
 @view_config(route_name='get_maker_detail', renderer='json')
 def get_maker_detail(request):
     try:
@@ -79,13 +88,46 @@ def get_maker_detail(request):
             for item in model.items:
                 installed = None
                 if item.installed_id:
-                    #installed_item = DBSession.query(Item).filter(Item.id==item.installed_id).one()
                     installed = item.installed_description
-                model_items.append({'item_id': item.id, 'item_name': item.name, 'serial': item.serial, 'vendor': item.vendor.name, 'support': item.support, 'support_end': str(item.support_end), 'status': item.status, 'asset_number': item.asset_number, 'description': item.description, 'installed': installed})
-            models.append({'id':model.id, 'model_name': model.name, 'description': '-', 'asset_items': model_items})
-        return {'name': maker.name, 'description': maker.description, 'models': models}
-    except Exception:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+
+                if item.name:
+                    name = item.name
+                elif not item.name:
+                    if item.parent:
+                        name = '({0})'.format(item.parent.name)
+                    else:
+                        name = '-'
+
+                model_items.append(
+                    {
+                        'item_id': item.id,
+                        'item_name': name,
+                        'serial': item.serial,
+                        'vendor': item.vendor.name,
+                        'support': item.support,
+                        'support_end': str(item.support_end),
+                        'status': item.status,
+                        'asset_number': item.asset_number,
+                        'description': item.description,
+                        'installed': installed
+                    }
+                )
+            models.append(
+                {
+                    'id':model.id,
+                    'model_name': model.name,
+                    'description': '-',
+                    'asset_items': model_items
+                }
+            )
+        return {
+            'name': maker.name,
+            'description': maker.description,
+            'models': models
+        }
+    except Exception as e:
+        return Response(conn_err_msg, content_type='text/plain',
+                        status_int=500)
 
 
 @view_config(route_name='get_model_detail', renderer='json')
@@ -95,10 +137,17 @@ def get_model_detail(request):
         model = DBSession.query(Model).filter(Model.id==model_id).one()
         items = []
         for item in model.items:
+            if item.name:
+                name = item.name
+            elif not item.name:
+                if item.parent:
+                    name = '({0})'.format(item.parent.name)
+                else:
+                    name = '-'
             items.append(
                 {
                     'id': item.id,
-                    'name': item.name,
+                    'name': name,
                     'serial': item.serial,
                     'vendor': {
                       'id': item.vendor.id,
@@ -115,10 +164,84 @@ def get_model_detail(request):
                     'updated_at': str(item.updated_at)
                 }
             )
-        return {'model_id': model_id, 'name': model.name, 'maker': {'id': model.maker.id, 'name': model.maker.name}, 'asset_items': items}
+        return {
+            'model_id': model_id,
+            'name': model.name,
+            'maker': {'id': model.maker.id, 'name': model.maker.name},
+            'asset_items': items
+        }
     except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+        return Response(conn_err_msg, content_type='text/plain',
+                        status_int=500)
 
+
+@view_config(route_name='get_asset_detail', renderer='json')
+def get_asset_detail(request):
+    try:
+        asset_id = request.matchdict['asset_id']
+        asset = DBSession.query(Item).filter(Item.id==asset_id).one()
+        related_assets = []
+        for attachment in asset.attachments:
+            print(attachment.installed_description)
+            related_assets.append(
+                {
+                    'id': attachment.id,
+                    'serial': attachment.serial,
+                    'installed_description': attachment.installed_description,
+                    'asset_number': attachment.asset_number,
+                    'updated_at': str(attachment.updated_at),
+                    'description': attachment.description,
+                    'maker': attachment.model.maker.name,
+                    'model': attachment.model.name
+                }
+            )
+        if asset.name:
+            name = asset.name
+        else:
+            if asset.parent:
+                name = '({0})'.format(asset.parent.name)
+            else:
+                name = None
+        return {
+            'model': {
+                'id': asset.model.id,
+                'name': asset.model.name,
+                'description': asset.model.description
+            },
+            'vendor': {
+                'id': asset.vendor.id,
+                'name': asset.vendor.name
+            },
+            'maker': {
+                'id': asset.model.maker.id,
+                'name': asset.model.maker.name
+            },
+            'asset': {
+                'id': asset.id,
+                'name': name,
+                'serial': asset.serial,
+                'installed': {
+                    'id': asset.installed_id,
+                    'description': asset.installed_description
+                },
+                'status': asset.status,
+                'location': {
+                    'id': asset.location.id,
+                    'name': asset.location.name
+                },
+                'asset_number': asset.asset_number,
+                'settlement_number': asset.settlement_number,
+                'delivery_date': str(asset.delivery_date),
+                'support': asset.support,
+                'support_end': str(asset.support_end),
+                'updated_at': str(asset.updated_at),
+                'description': asset.description
+            },
+            'installed_items': related_assets
+        }
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain',
+                        status_int=500)
 
 @view_config(route_name='index', renderer='templates/index.html')
 def index(request):
